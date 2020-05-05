cc05d5f8f08541941b1aaed4fab3ac574209cbf9
Tommy Yau <Tommy.Yau@Unilever.com>
2020-05-06 00:24:46 +0100

    Integration with Tab

---
+++ studio/parts/asset-sources.js
@@ -0,0 +1,4 @@
+import SanityAssetSource from 'part:@sanity/form-builder/input/image/asset-source-default';
+import TabAPISource from 'part:sanity-plugin-asset-source-tab/image-asset-source';
+
+export default [SanityAssetSource, TabAPISource];
